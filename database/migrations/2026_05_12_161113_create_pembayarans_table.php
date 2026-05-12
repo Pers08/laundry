fed5837ec343b915fef29bb1cde20c6aad2276e2
Pers08 <kevinsmile22022002@gmail.com>
2026-05-12 23:28:13 +0700

    Tambah migration tabel laundry: layanan, pelanggan, transaksi, detail transaksi, pembayaran

---
+++ database/migrations/2026_05_12_161113_create_pembayarans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaksi_id')->constrained('transaksis');
+            $table->date('tgl_bayar');
+            $table->decimal('total_bayar', 12, 2);
+            $table->string('metode_pembayaran'); // Cash atau Transfer
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
